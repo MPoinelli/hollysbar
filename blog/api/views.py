df61c7986a0030dd99c0bec3cf2dd419acd34dd3
Mattia Poinelli <m.poinelli@tudelft.nl>
2023-01-08 11:06:57 -0800

    Minor cosmetic edits

--- blog/api/views.py
+++ blog/api/views.py
@@ -6,67 +6,70 @@ from account.models import Account
 from blog.models import BlogPost
 from blog.api.serializers import BlogPostSerializer
 
+SUCCESS = 'success'
+ERROR = 'error'
+DELETE_SUCCESS = 'Post was deleted'
+UPDATE_SUCCESS = 'Update successful'
+CREATE_SUCCESS = 'Post was created'
+
 @api_view(['GET', ])
 def api_detail_blog_view(request, slug):
 
-    try:
-        blog_post = BlogPost.objects.get(slug=slug)
-    except BlogPost.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+	try:
+		blog_post = BlogPost.objects.get(slug=slug)
+	except BlogPost.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = BlogPostSerializer(blog_post)
-        return Response(serializer.data)
+	if request.method == 'GET':
+		serializer = BlogPostSerializer(blog_post)
+		return Response(serializer.data)
 
-@api_view(['PUT', ])
+
+@api_view(['PUT',])
 def api_update_blog_view(request, slug):
 
-    try:
-        blog_post = BlogPost.objects.get(slug=slug)
-    except BlogPost.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+	try:
+		blog_post = BlogPost.objects.get(slug=slug)
+	except BlogPost.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'PUT':
-        
-        serializer = BlogPostSerializer(blog_post, data=request.data)
-        data = {}
-        if serializer.is_valid():
-            serializer.save()
-            data["success"] = "update successful"
-            return Response(data=data)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	if request.method == 'PUT':
+		serializer = BlogPostSerializer(blog_post, data=request.data)
+		data = {}
+		if serializer.is_valid():
+			serializer.save()
+			data[SUCCESS] = UPDATE_SUCCESS
+			return Response(data=data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['DELETE', ])
+@api_view(['DELETE',])
 def api_delete_blog_view(request, slug):
 
-    try:
-        blog_post = BlogPost.objects.get(slug=slug)
-    except BlogPost.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+	try:
+		blog_post = BlogPost.objects.get(slug=slug)
+	except BlogPost.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'DELETE':
-        operation = blog_post.delete()
-        data = {}
+	if request.method == 'DELETE':
+		operation = blog_post.delete()
+		data = {}
+		if operation:
+			data[SUCCESS] = DELETE_SUCCESS
+		return Response(data=data)
 
-        if operation:
-            data["success"] = "delete successful"
-        else:
-            data["failure"] = "delete failed"
-        
-        return Response(data=data)
 
-@api_view(['POST', ])
+@api_view(['POST'])
 def api_create_blog_view(request):
 
-    account = Account.objects.get(pk=1)
+	account = Account.objects.get(pk=1)
 
-    blog_post = BlogPost(author=account)
+	blog_post = BlogPost(author=account)
 
-    if request.method == "POST":
-        serializer = BlogPostSerializer(blog_post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	if request.method == 'POST':
+		serializer = BlogPostSerializer(blog_post, data=request.data)
+		data = {}
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
