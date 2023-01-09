3d83853c2e1c965d0049aa74444e5e39941c6113
Mattia Poinelli <m.poinelli@tudelft.nl>
2023-01-09 13:48:19 -0800

    Set up authentication token 2

--- account/api/views.py
+++ account/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.authtoken.models import Token
 
 from account.api.serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 
 
 @api_view(['POST'])
